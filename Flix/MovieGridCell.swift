6b94feff70a7dc2414fa57bd2b545e08b2bb81ea
Jason Chan <jasonchan.newyork@gmail.com>
2021-02-21 01:02:03 -0500

    finished part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jason  Chan on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
